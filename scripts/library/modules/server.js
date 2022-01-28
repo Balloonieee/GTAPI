3bea769dd25cf49796c361e88eced4bf5ba858c9
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2022-01-28 11:20:07 -0500

    Update server.js

--- scripts/library/modules/server.js
+++ scripts/library/modules/server.js
@@ -1,6 +1,28 @@
 
 class serverManager {
+  constructor() {
+  }
   
+  init() {
+    
+  }
+
+  getPlayers(type) {
+    const validTypes = ['list', 'object']
+    if(!validTypes.includes(type)) 
+      throw new Error(`${type} is not a valid type`)
+    
+    return type == 'object' ? World.getPlayers().Map(mP => new player(mP)) : World.getPlayers().Map(mP => { name: mP?.name, nameTag: mP?.nameTag })
+  }
+  
+ /* getDimension({ dimension }) {
+    const dimensions = ['overworld','the end','nether','all']
+    if(!dimensions.includes(dimension)) 
+      throw new Error(`${dimension} is not a valid dimension`)
+    
+    return dimension == 'all' ? {
+      overworld: new Dimension('overworld'), nether: new Dimension('nether'), end: new Dimension('the end')
+    } : World.getDimension('dimension')*/
 }
 
 export const server = new serverManager()
