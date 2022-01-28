303b2bcd925868a504137da573194e44b18a5c1b
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2022-01-28 13:33:13 -0500

    Update dimension.js

--- scripts/library/modules/dimension.js
+++ scripts/library/modules/dimension.js
@@ -1,1 +1,15 @@
+import { world } from 'mojang-minecraft'
 
+export class dimension {
+  constructor(dimensionId) {
+    const validDimensions = ['overworld', 'nether', 'the end', 'all']
+    if(!validDimensions.includes(dimensionId)) 
+      throw new Error(`${dimensionId} is not a valid dimension`)
+    
+    const Mdimension = dimensionId == 'all' ? validDimensions.Map(dimensionId => {
+      if(dimensionId == 'all') return
+      return world.getDimension(dimensionId)
+    }) : [world.getDimension(dimensionId)] 
+  }
+  
+}
