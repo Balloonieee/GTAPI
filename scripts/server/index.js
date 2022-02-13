70943157a98778b8b67c011cd17a42eb38c20679
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2022-02-13 17:29:48 -0500

    Update index.js

--- scripts/server/index.js
+++ scripts/server/index.js
@@ -1,13 +1,9 @@
 import { server } from '../library/GTAPI.js'
 import { world } from 'mojang-minecraft'
 
-server.events.on('playerJoin', (JoinData) => console.warn(JoinData.player.getName() ?? JoinData.player.getNameTag()))
-
-server.events.on('playerLoad', (JoinData) => console.warn(JoinData.player, JoinData.tickToLoad))
-
 //server.events.on('tick', (tickData) => console.warn(tickData))
 
-world.events.beforeChat.subscribe(data => {
-if(data.message == 'tp') server.players.getPlayers()[0]?.teleport({ location: [5, 20, 69], dimension: 'overworld', facingLocation: [60, 30, 70] })
-})
+server.events.on('beforeChat', beforeChatPacket => console.warn(beforeChatPacket.message))
 
+server.events.on('playerJoin', (joinData) => console.warn(joinData.player.getName() ?? joinData.player.getNameTag()))
+server.events.on('playerLoad', (loadData) => console.warn(loadData.player.getName() ?? joinData.player.getNameTag(), JoinData.tickToLoad))
