ec7137220ac55b9d23bbc28aaf3fcbd212d483b8
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2022-02-17 21:53:52 -0500

    Update index.js

--- scripts/server/index.js
+++ scripts/server/index.js
@@ -6,7 +6,7 @@ import { world } from 'mojang-minecraft'
 server.events.on('beforeChat', beforeChatPacket => console.warn(beforeChatPacket.message))
 
 server.events.on('playerJoin', (joinData) => console.warn(joinData.player.getName() ?? joinData.player.getNameTag()))
-server.events.on('playerLoad', (loadData) => console.warn(loadData.player.getName() ?? joinData.player.getNameTag(), JoinData.tickToLoad))
+server.events.on('playerLoad', (loadData) => console.warn(loadData.player.getName() ?? loadData.player.getNameTag(), loadData.tickToLoad))
 
 server.events.on('beforeItemUse', (beforeItemUseData) => console.warn('beforeitemuse'))
 server.events.on('beforeItemUseOn', (beforeItemUseData) => console.warn('beforeitemuseon'))
